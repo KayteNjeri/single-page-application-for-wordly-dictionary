69fbb601ef9b085cb17c21dc04792ded4096c385
Catherine <canjeri91@gmail.com>
2026-05-02 21:31:08 +0300

    Wordly dictionary

--- README.md
+++ README.md
@@ -1,40 +1,57 @@
 # A Single Page Application for Wordly Dictionary
-A Single Page Application is a responsive, front-end web application designed to allow users to search for words, show the word's definitions, it's pronunciation, synonyms and source details. It is built using web technologies and integrates with https://dictionaryapi.dev/.
-# Features
-**Word Search:** Search for English words by inputting a word.
-**Definitions:** Shows the word's meaning, it's pronunciation, how it is used, it's synonyms (related words) and also provide an audio playback for pronunciation when available.
-**Favorite Words** Save favorite words for any future reference. The favorite words will be stored locally in the browser.
-**Responsive Design** Built with modern CSS techniques including Grid and Flexbox, featuring a clean, card-based layout for a responsive and user-friendly interface.
-# Technologies Used
--**HTML5:** Semantic structure.
--**CSS:** Modern styling - Flexbox, CSS Grid
--**JavaScript:** Fetch data from the API (https://dictionaryapi.dev/)
-Displaying fetched data
-Handling user input and events
 
-# Getting Started
+A Single Page Application is a responsive, front-end web application designed to allow users to search for words, show the word's definitions, it's pronunciation, synonyms and source details. It is built using web technologies and integrates with [Free Dictionary API](https://dictionaryapi.dev/).
+
+## Features
+
+- **Word Search:** Search for English words by inputting a word.
+- **Definitions:** Shows the word's meaning, it's pronunciation, how it is used, it's synonyms (related words) and also provide an audio playback for pronunciation when available.
+- **Favorite Words** Save favorite words for any future reference. The favorite words will be stored locally in the browser.
+- **Responsive Design** Built with modern CSS techniques including Grid and Flexbox, featuring a clean, card-based layout for a responsive and user-friendly interface.
+
+## Technologies Used
+
+- **HTML5:** Semantic structure.
+- **CSS:** Modern styling - Flexbox, CSS Grid
+- **JavaScript:** Fetch data from the API (https://dictionaryapi.dev/)
+ - Displaying fetched data
+ - Handling user input and events
+
+## Getting Started
+
 Running the project is pretty straightforward.
 
-# Prerequisites
+## Prerequisites
+
 You need a browser (Chrome, Mozilla, Edge) to run this application.
-# Installation
-1. Clone the repository
-https://github.com/KayteNjeri/single-page-application-for-wordly-dictionary/tree/main
+## Installation
+
+1. Clone the repository:
+
+   ```bash
+git clone https://github.com/KayteNjeri/single-page-application-for-wordly-dictionary
+   ```
+
 2. Navigate to the project directory:
-   cd ............
-3. Simply open the index.html file in your browser. Consider having an extension of live server in VS Code in order to run the project over a local HTTP server.
 
-# Project Structure
-main.js
-index.html
-style.css
+   ```bash
+   cd single-page-application-for-wordly-dictionary
+   ```
 
-# API Reference
+3. Simply open the `index.html` file in your browser. Consider having an extension of **Live Server** in VS Code in order to run the project over a local HTTP server.
+
+## Project Structure
+
+- `index.html`: This is the main HTML structure, which includes ..............
+- `main.js`: This is the core application logic handling the API calls, ........................
+- `style.css`: This is used for styling...................
+
+## API Reference
 This application uses the free Dictionary API [..................](https://dictionaryapi.dev/)
 
-# Future Improvements
-Save favorite words in a local storage
+## Future Improvements
+- [ ] Save favorite words in a local storage
 
-# Licence
+## Licence
 This project is open source and available for educational purposes.
 
---
+++ main.js
@@ -0,0 +1,167 @@
+// Elements
+const searchForm = document.getElementById("search-form");
+const searchInput = document.getElementById("word-input");
+//create elements for output
+const output = document.getElementById("output");
+const wordOutput = document.getElementById("word");
+const wordPhonetic = document.getElementById("phonetic");
+const wordDefinition = document.getElementById("definition");
+const wordAudio = document.getElementById("audio");
+const wordSynonym = document.getElementById("synonyms");
+
+const saveButton = document.getElementById("saveButton");
+//create element for favorites
+const favoriteList = document.getElementById("favoriteWords");
+
+let currentWord="";
+let favorites = JSON.parse(localStorage.getItem("favorites")) || [];
+
+//Access API data
+async function searchWords(word) {
+    try {
+        const response = await fetch(`https://api.dictionaryapi.dev/api/v2/entries/en/${word}`);
+
+        if (!response.ok) throw new Error("Word not found");
+
+        const data = await response.json();
+        return data[0];
+
+    } catch (error) {
+        console.error("Error fetching the word:", error);
+        return null;
+    }
+}
+
+//display data - output container
+function displayWord(data) {
+   if(!data) { 
+    output.classList.remove("hidden");
+    //output.innerHTML = "<p>The word has not been found.</p>";
+    return;
+   }
+
+   currentWord = data.word;  
+
+   wordOutput.textContent = data.word;
+   //phonetic
+   wordPhonetic.textContent = data.phonetic || "No pronunciation available";
+   //definition
+   wordDefinition.innerHTML = "<h3>Meanings</h3>"
+        const meanings = data.meanings || [];
+        meanings.forEach(meaning => {
+            const block = document.createElement("div");
+            block.className = "meaning-block";
+
+            const partOfSpeech = document.createElement("h4");
+            partOfSpeech.textContent = meaning.partOfSpeech || "";
+            block.append(partOfSpeech);
+
+        const definitions = meaning.definitions || [];
+
+            definitions.forEach(def => {
+                const defItem = document.createElement("p");
+            defItem.innerHTML = `
+            <strong>Definition:</strong> ${def.definition || ""}
+             ${def.example ? `<br><em>Example: ${def.example}</em>` : ""}
+         `;
+
+         block.append(defItem);
+      });
+      wordDefinition.append(block);
+    });
+
+   //synonym
+   wordSynonym.innerHTML ="<h3>Synonyms</h3>";
+   const synonyms = data.meanings
+    .flatMap(m => m.definitions || [])
+    .flatMap(d => d.synonyms || []);
+
+    if (synonyms.length > 0) {
+        synonyms.forEach(s => {
+            const span = document.createElement("span");
+            span.textContent = s;
+            span.className = "synonym";
+            wordSynonym.appendChild(span);
+        });
+    } else {
+        wordSynonym.innerHTML += "<p>Synonyms not available</p>";
+    }
+    
+   //audio
+   wordAudio.innerHTML = "";
+   const audioURL = data.phonetics?.find(p => p.audio)?.audio;
+
+   if(audioURL) {
+    const btn = document.createElement("button");
+    btn.textContent = "Play";
+    
+    btn.addEventListener("click", () => {
+        new Audio(audioURL).play();
+        });
+
+    wordAudio.append(btn);
+    }
+
+    output.classList.remove("hidden");
+}
+
+//save favorite words
+function renderFavoriteWords(){
+    favoriteList.innerHTML = "";
+
+    if (favorites.length === 0) {
+        favoriteList.innerHTML = "<li>No favorite words yet!</li>";
+        return;
+    }
+    favorites.forEach(word => {
+        const term = document.createElement("li");
+        term.className = "favorite-item";
+        term.innerHTML = `
+        <span>${word}</span>
+        <button class="remove-fav-button">✕</button>`;
+
+        term.querySelector("span").onclick = () => searchAndGiveOutput(word);
+
+        term.querySelector("button").onclick = () => {
+            favorites = favorites.filter(w => w !== word);
+            localStorage.setItem("favorites", JSON.stringify(favorites));
+            renderFavoriteWords();
+        };
+        favoriteList.append(term);
+    });
+}
+
+async function searchAndGiveOutput(word) {
+    const data = await searchWords(word);
+    console.log("API DATA:", data);
+    displayWord(data);
+}
+
+//submit the form
+searchForm.addEventListener("submit", async (event) => {
+    event.preventDefault()
+
+    const word = searchInput.value.trim();
+    if(!word) return ;
+
+    output.classList.remove("hidden");
+   // output.innerHTML = "<p>Loading the word...</p>";
+    
+
+    await searchAndGiveOutput(word);
+    
+    searchInput.value = "";
+});
+
+//add event listener to save favorites
+saveButton.addEventListener("click", () => {
+    if (!currentWord) return;
+
+    if (!favorites.includes(currentWord)) {
+        favorites.push(currentWord);
+        localStorage.setItem("favorites", JSON.stringify(favorites));
+        renderFavoriteWords();
+    }
+});
+
+renderFavoriteWords();
